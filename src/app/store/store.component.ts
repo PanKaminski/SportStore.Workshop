0f70cca015966b95a6e974f92dbabc93fbfd1822
KaminsLab <55655435+KaminsLab@users.noreply.github.com>
2022-04-13 17:51:02 +0300

    Add pagination, add directive.

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -7,13 +7,40 @@ import { ProductRepository } from "../model/product.repository";
     templateUrl: "store.component.html"
 })
 export class StoreComponent{
+    public selectedCategory: string | null = null;
+    public productsPerPage: number = 4;
+    public selectedPage: number = 1;
+
     constructor(private repository: ProductRepository){ }
 
     get products(): Product[] {
-        return this.repository.getProducts();
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories() : string[] {
         return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory: string | null = null) {
+        this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newPageSize: number) {
+        this.productsPerPage = Number(newPageSize);
+        this.changePage(1);
+    }
+
+    get pageCount(): number{
+        return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)
+    }
+
+    get pageNumbers(): number[] {
+        return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
+        .fill(0).map((x, i) => i + 1);
     }
 }
